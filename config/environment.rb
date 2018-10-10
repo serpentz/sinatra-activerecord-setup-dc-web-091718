7049701f681a6479d0b9e8605abfc9e3edfd114e
serpentz <absalomcherinet@gmail.com>
2018-10-10 10:00:01 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
+# require 'sinatra/activerecord/rake'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
 
 require './app'
